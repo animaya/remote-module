a9b7c8201d8cfccc6b50e6c963295087e7eca95d
animaya <46826986+animaya@users.noreply.github.com>
2024-03-02 19:47:43 +0300

    Update remote-module.go

--- remote-module.go
+++ remote-module.go
@@ -1,8 +1,9 @@
 package remotemodule
 
-func Reverse(text string) {
+func Reverse(text string) string {
 	result := ""
 	for _, char := range text {
 		result = string(char) + result
 	}
+	return result
 }
